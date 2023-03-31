e0cc80bc84c11226dea513b5e38f524bf07461f8
Nelson Hereveri <nelson@hereveri.cl>
2023-03-31 20:28:08 -0300

    Mejorando FruitCell

--- CoolFruits/TableView/FruitCell.swift
+++ CoolFruits/TableView/FruitCell.swift
@@ -12,21 +12,28 @@ import UIKit
 public class FruitCell: UITableViewCell {
   @IBOutlet weak var fruitNameLabel: UILabel!
   @IBOutlet weak var fruitSugarLabel: UILabel!
+  
   public override func awakeFromNib() {
     super.awakeFromNib()
   }
   
   public func setUp(fruitName: String?, fruitSugar: Double?, isCitrus: Bool) {
-    guard let fruitName = fruitName else {
+    guard let fruitName = fruitName, let fruitSugar = fruitSugar else {
       return
     }
     
-    guard let fruitSugar = fruitSugar else {
-      return
-    }
-    
-    fruitNameLabel.text = "\(isCitrus ? " * " : "") \(fruitName)"
-    fruitSugarLabel.text = "(Sugar:" + String(fruitSugar) + ")"
+    fruitNameLabel.text = formattedFruitName(isCitrus: isCitrus, fruitName: fruitName)
+    fruitSugarLabel.text = formattedSugarText(fruitSugar: fruitSugar)
+  }
+}
+
+extension FruitCell {
+  func formattedFruitName(isCitrus: Bool, fruitName: String) -> String {
+    return "\(isCitrus ? " * " : "") \(fruitName)"
+  }
+  
+  func formattedSugarText(fruitSugar: Double) -> String {
+    return "(Sugar:" + String(fruitSugar) + ")"
   }
   
   class func createCell() -> FruitCell? {
